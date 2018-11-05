168062686f218ff099ea5f9749ae4ea55d3706bb
reck1ess <heeinso@snu.ac.kr>
2018-11-05 16:48:37 +0900

    feat: Implement Deck component

---
+++ components/Deck.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+import {
+	Animated,
+	StyleSheet,
+	Text,
+	TouchableOpacity,
+	View,
+} from 'react-native';
+import { Button, Card, FormValidationMessage } from 'react-native-elements';
+
+import { getDeck } from '../utils/api';
+
+class Deck extends Component {
+	state = {
+		deck: null,
+		animation: new Animated.Value(0),
+	};
+
+	componentDidMount() {
+		const { title } = this.props.navigation.state.params;
+		this.initialize(title);
+	}
+
+	initialize(title) {
+		getDeck(title).then(deck => {
+			this.setState({
+				deck,
+			});
+		});
+	}
+
+	addCard() {
+		const { navigation } = this.props;
+		const { title, updateDeck } = this.props.navigation.state.params;
+		navigation.navigate('AddCard', { title, updateDeck });
+	}
+
+	startQuiz() {
+		const cardCount = this.state.deck.cards.length;
+		const { navigation } = this.props;
+		const { title } = this.props.navigation.state.params;
+
+		cardCount !== 0 &&
+			navigation.navigate('Quiz', {
+				title,
+			});
+	}
+
+	renderDeck() {
+		const { deck, animation } = this.state;
+		const cardCount = deck.cards.length;
+
+		return (
+			<Card title={deck.title}>
+				<Animated.Text
+					style={[styles.cardCount, { fontSize: animation }]}>
+					{cardCount}
+				</Animated.Text>
+				<Text style={styles.cardCountText}>
+					{cardCount > 1 ? ' cards' : ' card'}
+				</Text>
+				<Button
+					icon={{ name: 'add' }}
+					backgroundColor="#03A9F4"
+					buttonStyle={styles.addCardButton}
+					title="ADD CARD"
+					onPress={() => this.addCard()}
+				/>
+				<Button
+					icon={{ name: 'done' }}
+					backgroundColor="#009689"
+					buttonStyle={styles.startQuizButton}
+					title="START QUIZ"
+					onPress={() => this.startQuiz()}
+				/>
+				{cardCount == 0 ? (
+					<FormValidationMessage>
+						{'You cannot start quiz without card decks'}
+					</FormValidationMessage>
+				) : (
+					<FormValidationMessage />
+				)}
+			</Card>
+		);
+	}
+	render() {
+		const { deck, animation } = this.state;
+
+		Animated.timing(animation, {
+			toValue: 50,
+			duration: 1000,
+		}).start();
+
+		return deck ? this.renderDeck() : <View />;
+	}
+}
+
+const styles = StyleSheet.create({
+	cardCount: {
+		fontWeight: '500',
+		textAlign: 'center',
+		marginBottom: 20,
+	},
+	cardCountText: {
+		textAlign: 'center',
+		marginBottom: 40,
+	},
+	addCardButton: {
+		borderRadius: 0,
+		marginLeft: 0,
+		marginRight: 0,
+		marginBottom: 20,
+	},
+	startQuizButton: {
+		borderRadius: 0,
+		marginLeft: 0,
+		marginRight: 0,
+		marginBottom: 0,
+	},
+});
+
+export default Deck;
